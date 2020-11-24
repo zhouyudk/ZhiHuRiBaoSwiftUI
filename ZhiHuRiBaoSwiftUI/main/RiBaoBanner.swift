3fab6b790ae1e0ee41ce199b0f394caceda96085
zhouyudk <yu.zhou@razer.com>
2020-11-24 18:12:50 +0800

    add timer

--- ZhiHuRiBaoSwiftUI/main/RiBaoBanner.swift
+++ ZhiHuRiBaoSwiftUI/main/RiBaoBanner.swift
@@ -10,11 +10,62 @@ import SwiftUI
 struct RiBaoBanner: View {
     @ObservedObject var todayNews: TodayNewsModel
     @State var index = 0
+    @State private var scrollTimer: Timer?
+    @State private var isLeftToRight = true
+    #warning("定制轮播器")
     var body: some View {
-        SwiftUIPagerView(index: $index, pages: todayNews.top_stories.map({
-            BannerItem(itemData: $0, id: $0.id)
-        }))
-        .frame(width: UIScreen.screenWidth, height: 400)
+        ZStack(alignment: .center){
+            SwiftUIPagerView(index: $index, pages: todayNews.top_stories.map({
+                BannerItem(itemData: $0, id: $0.id)
+            }))
+            .frame(width: UIScreen.screenWidth, height: 400)
+            .onAppear(perform: {
+                index = todayNews.top_stories.count
+//                enableAutoScroll()
+            })
+            .onDisappear(perform: {
+                disableAutoScroll()
+            })
+            Text("\(index)")
+        }
+
+    }
+    
+    func enableAutoScroll() {
+        guard todayNews.top_stories.count > 1 else {
+//            pageControl.isHidden=true
+//            collectionView.isScrollEnabled = false
+            return
+        }
+//        pageControl.isHidden = false
+//        collectionView.isScrollEnabled = true
+        let t = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { (_) in
+            if isLeftToRight {
+                if index+1 < todayNews.top_stories.count {
+                    index += 1
+                } else {
+                    isLeftToRight = false
+                    index -= 1
+                }
+            } else {
+                if index-1 >= 0 {
+                    index -= 1
+                } else {
+                    isLeftToRight = true
+                    index += 1
+                }
+            }
+
+        }
+        scrollTimer = t
+        RunLoop.main.add(scrollTimer!, forMode: RunLoop.Mode.common)
+    }
+
+    func disableAutoScroll() {
+        if self.scrollTimer != nil {
+            self.scrollTimer?.invalidate()
+            self.scrollTimer = nil
+        }
     }
 }
 
